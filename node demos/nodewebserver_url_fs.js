fd65528856bb4659d85c3495f653eeba33d79d3f
unknown <joshipriyankaatul@gmail.com>
2023-08-11 21:00:26 +0530

    first

---
+++ node demos/nodewebserver_url_fs.js
@@ -0,0 +1,18 @@
+var h = require('http');
+var u = require('url');
+var fs = require('fs');
+var server = h.createServer(function(req,res){
+	var url_p = u.parse(req.url, true);
+	var path = url_p.pathname;   //    /home.html		
+	var fname = path.substring(1);   //  home.html
+	fs.readFile(fname, function(err,data) {
+	   if(!err)
+	   {
+		res.writeHead(200,{'content-type':'text/html'});
+	 	res.write(data.toString());	
+		res.end();		
+	   }
+       });
+});
+
+server.listen(9000,function() {console.log("SERVER STARted")})
